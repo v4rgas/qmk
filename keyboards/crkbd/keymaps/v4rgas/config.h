a8d92ec9274f5e2a72b6afdb5fda3576ade5580f
v4rgas <66626747+v4rgas@users.noreply.github.com>
2025-03-28 13:23:18 -0300

    Refactor keymap and configuration for improved RGB matrix support and OLED settings

--- keyboards/crkbd/keymaps/v4rgas/config.h
+++ keyboards/crkbd/keymaps/v4rgas/config.h
@@ -20,10 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-// #define USE_MATRIX_I2C
-
-// #define QUICK_TAP_TERM 0
-// #define TAPPING_TERM 100
+#define EE_HANDS
+#define SPLIT_USB_DETECT
 
 #ifdef OLED_ENABLE
 #    define SPLIT_OLED_ENABLE
@@ -31,22 +29,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define SPLIT_LED_STATE_ENABLE
 #    define SPLIT_WPM_ENABLE
 #    define SPLIT_MODS_ENABLE
-# define OLED_FONT_H "keyboards/crkbd/keymaps/v4rgas/glcdfont.c"
+#    define OLED_FONT_H "keyboards/crkbd/keymaps/v4rgas/glcdfont.c"
 #endif
 
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
-#    define RGBLIGHT_LIMIT_VAL 120
-#    define RGBLIGHT_HUE_STEP 10
-#    define RGBLIGHT_SAT_STEP 17
-#    define RGBLIGHT_VAL_STEP 17
+#ifdef RGB_MATRIX_ENABLE
+#    define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+#    define RGB_MATRIX_SLEEP // turn off effects when suspended
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// enabled only of RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is defined
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#    define ENABLE_RGB_MATRIX_SPLASH
+#    define ENABLE_RGB_MATRIX_MULTISPLASH
+#    define ENABLE_RGB_MATRIX_SOLID_SPLASH
+#    define ENABLE_RGB_MATRIX_SOLID_MULTISPLASHsss
 #endif
